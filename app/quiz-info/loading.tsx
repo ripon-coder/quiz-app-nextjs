1ee78c4e2789700184e47e2661944c98f0b03a72
Ripon Shikder <coderripon@gmail.com>
2025-08-16 18:30:43 +0600

    loading

--- app/quiz-info/loading.tsx
+++ app/quiz-info/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="bg-black p-4">
+    <div className="bg-black p-2">
       <div className="w-full h-screen bg-[#382e2e] animate-pulse" />
     </div>
   );
